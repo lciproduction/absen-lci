a3c8d0cab0b6176e4567c86cabaa04fc1a703597
rhaihanadzani <mrhaihan17@gmail.com>
2024-10-27 19:51:44 +0700

    add logooo

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -7,7 +7,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>@yield('title', config('app.name', 'Laravel')) - {{ config('app.name', 'Laravel') }}</title>
-    <link rel="shortcut icon" href="{{ asset('assets/images/simanjaterbaru.png') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('assets/images/logoo.png') }}" type="image/x-icon">
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
