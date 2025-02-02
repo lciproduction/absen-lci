0d4a159e4bf75e95f4cf91f686cf26a060b304d5
rhaihanadzani <mrhaihan17@gmail.com>
2025-02-03 00:57:03 +0700

    testing ajah inimah

--- resources/views/student/attendance/index.blade.php
+++ resources/views/student/attendance/index.blade.php
@@ -15,9 +15,16 @@
                 <div class="flex flex-col p-2 lg:p-6 text-center w-full ">
 
                     @include('student.attendance.selector')
-                    <p id="demo"></p>
-                    <button onclick="initializeLocation()">klik</button>
+                    {{-- <p id="demo"></p>
+                    <button onclick="getNavigator()">klik</button> --}}
                     <div id="locationIframe"></div>
+                    <div id="skeletonMap" class="hidden">
+
+                        <iframe class="bg-gray-200 skeleton " width="100%" height="300" frameborder="0"
+                            allowfullscreen></iframe>
+                        <button
+                            class="btn bg-red-secondary hover:bg-red-primary/80 text-white skeleton w-24 mt-5"></button>
+                    </div>
                     <span class="loading loading-spinner text-primary hidden" id="loading"></span>
 
                     <!-- Buttons for Absen Masuk (Hadir) and Absen Pulang -->
@@ -40,42 +47,220 @@
         </div>
     </div>
     <script>
+        document.addEventListener('DOMContentLoaded', function() {
+
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+
+            const status = document.getElementById('status');
+            const userId = '{{ auth()->user()->student?->id }}';
+            const apiUrl = '{{ route('student.attendance.store') }}';
+            const roleId = '{{ auth()->user()->role }}';
+            const locationIframe = document.getElementById('locationIframe');
+
+            status.addEventListener('change', handleStatusChange);
+
+            function handleStatusChange() {
+                const statusValue = status.value;
+                hideAll();
+                switch (statusValue) {
+                    case 'AbsenHadir':
+                        showLocation();
+                        break;
+
+                    case 'AbsenPulang':
+                        showLocationPulang();
+                        break;
+
+
+                    case 'Izin':
+                        showPermit();
+                        break;
+
+
+                    case 'Sakit':
+                        showSick();
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            function hideAll() {
+                $('#fileUpload, #permit, #AbsenHadir, #AbsenPulang, #sendPermit, #sendSick, #toast-top-left')
+                    .hide();
+                locationIframe.innerHTML = '';
+            }
+
+            function showLocation() {
+                // $('#AbsenHadir').show(); // Show the WFO button correctly
+                $('#skeletonMap').show(); // Show the WFO button correctly
+                initializeLocation();
+            }
+
+
+            function showLocationPulang() {
+                $('#AbsenPulang').show();
+                initializeLocationPulang();
+            }
+
+            function showPermit() {
+                $('#permit, #sendPermit').show();
+                initializePermit();
+            }
+
+            function showSick() {
+                $('#fileUpload, #sendSick').show();
+                initializeSick();
+            }
+
+            function initializeLocation() {
+                const btn = document.getElementById('AbsenHadir');
+                if (navigator.geolocation) {
+                    navigator.geolocation.getCurrentPosition(
+                        position => {
+                            const latitude = position.coords.latitude;
+                            const longitude = position.coords.longitude;
+                            const formData = {
+                                id: userId,
+                                role: roleId,
+                                latitude: latitude,
+                                longitude: longitude,
+                                status: status.value,
+                            };
+
+                            displayLocationIframe(latitude, longitude); // Show the map iframe
+
+                            console.log("Latitude: " + latitude);
+                            $('#skeletonMap').hide();
+
+                            $('#AbsenHadir').show();
+                            btn.onclick = function() {
+                                sendFormData(formData);
+                            };
+                        },
+                        error => {
+                            console.error('Geolocation error:', error);
+                            $('#note-error').show();
+                        }, {
+                            maximumAge: 600_000
+                        }
+                    );
+                } else {
+                    $('#note-error').show();
+                    console.log('Geolocation is not supported by this browser.');
+                }
+            }
+
+            function initializeLocationPulang() {
+                const btn = document.getElementById('AbsenPulang');
+                navigator.geolocation.getCurrentPosition(
+                    position => {
+                        const latitude = position.coords.latitude;
+                        const longitude = position.coords.longitude;
+                        const formData = {
+                            id: userId,
+                            role: roleId,
+                            latitude: latitude,
+                            longitude: longitude,
+                            status: status.value,
+                        };
+
+                        // displayLocationIframe(latitude, longitude); // Show the map iframe
+
+                        btn.onclick = function() {
+                            sendFormData(formData);
+                        };
+                    },
+                    error => {
+                        console.error('Geolocation error:', error);
+                        $('#note-error').show();
+                    }
+                );
+            }
+
+
+            function initializePermit() {
+                const btnIzin = document.getElementById('sendPermit');
+                btnIzin.onclick = function() {
+                    sendFormData({
+                        id: userId,
+                        status: status.value,
+                        izin: document.getElementById('izin').value
+                    });
+                };
+            }
+
+            function initializeSick() {
+                const btnSakit = document.getElementById('sendSick');
+                btnSakit.onclick = function() {
+                    const formData = new FormData();
+                    const fileInput = document.getElementById('sickFile');
+                    formData.append('id', userId);
+                    formData.append('status', status.value);
+                    if (fileInput.files.length > 0) {
+                        formData.append('file', fileInput.files[0]);
+                    }
+                    sendFormData(formData, true);
+                };
+            }
+
+
+            function displayLocationIframe(latitude, longitude) {
+
+                console.log("Latitude: " + latitude + " Longitude: " + longitude);
+                const googleMapsUrl =
+                    `https://www.google.com/maps?q=${latitude},${longitude}&hl=es;z=14&output=embed`;
+                locationIframe.innerHTML =
+                    `<iframe width="100%" height="300" src="${googleMapsUrl}" allow="geolocation" frameborder="0" a allowfullscreen></iframe>`;
+            }
+
+            function sendFormData(data, isFile = false) {
+                hideAll();
+                $('#loading').show();
+                $.ajax({
+                    url: apiUrl,
+                    method: 'POST',
+                    data: data,
+                    processData: !isFile,
+                    contentType: isFile ? false : 'application/x-www-form-urlencoded; charset=UTF-8',
+
+                    success: function(response) {
+                        console.log('Response:', response); // Log response
+                        $('#notif').text(response.message);
+                        $('#toast-top-left').addClass("block").removeClass("hidden").show();
+                    },
+                    error: function(error) {
+                        console.error('Error:', error);
+                        $('#notif').text(error.responseJSON.message);
+                        $('#toast-top-left').addClass("block").removeClass("hidden").show();
+                    },
+                    complete: function() {
+                        $('#loading').hide();
+                        status.value = '';
+                        document.getElementById('izin').value = '';
+                    }
+                });
+            }
+        });
         const x = document.getElementById("demo");
 
-        function initializeLocation() {
-            // const btn = document.getElementById('AbsenHadir');
+        function getNavigator() {
+            console.log("Initialize Location");
 
+            const a = navigator.geolocation;
+
+            console.log(a);
             if (navigator.geolocation) {
                 navigator.geolocation.getCurrentPosition(showPosition)
             }
-            // navigator.geolocation.getCurrentPosition(
-            //     position => {
-            //         const latitude = position.coords.latitude;
-            //         const longitude = position.coords.longitude;
-            //         const formData = {
-            //             id: userId,
-            //             role: roleId,
-            //             latitude: latitude,
-            //             longitude: longitude,
-            //             status: status.value,
-            //         };
-
-            //         // displayLocationIframe(latitude, longitude); // Show the map iframe
-
-            //         console.log("Latitude: " + latitude);
-
-            //         btn.onclick = function() {
-            //             sendFormData(formData);
-            //         };
-            //     },
-            //     error => {
-            //         console.error('Geolocation error:', error);
-            //         $('#note-error').show();
-            //     }
-            // );
         }
 
         function showPosition(position) {
+            console.log("Latitude: " + position.coords.latitude);
             x.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords
                 .longitude;
         }
