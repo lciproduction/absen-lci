46d6b2db798500938617bff48848d9c95c989bfd
rhaihanadzani <mrhaihan17@gmail.com>
2025-02-05 17:58:26 +0700

    add schedule

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,4 @@ use Illuminate\Support\Facades\Schedule;
 //     $this->comment(Inspiring::quote());
 // })->purpose('Display an inspiring quote')->hourly();
 
-Schedule::command('mark:absentees')->dailyAt('17:50')->timezone('Asia/Jakarta');
+Schedule::command('mark:absentees')->dailyAt('18:00')->timezone('Asia/Jakarta');
